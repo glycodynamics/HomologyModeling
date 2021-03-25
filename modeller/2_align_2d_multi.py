48ff36e8e3526d0e784c5b323f62741ff87f60ba
Sushil <10772897+glycodynamics@users.noreply.github.com>
2021-03-25 14:57:30 -0500

    Update 2_align_2d_multi.py

--- modeller/2_align_2d_multi.py
+++ modeller/2_align_2d_multi.py
@@ -16,5 +16,6 @@ aln.append(file='Query.ali', align_codes='Query')
 # Structure sensitive variable gap penalty sequence-sequence alignment:
 aln.align2d(overhang=0, gap_penalties_1d=(-100, 0),
             gap_penalties_2d=(3.5, 3.5, 3.5, 0.2, 4.0, 6.5, 2.0, 0., 0.),
+            align_block=aln_block)
 aln.write(file='query-mult.ali', alignment_format='PIR')
 aln.write(file='query-mult.pap', alignment_format='PAP')
